429833fa709d44c7c99e24b527b8bbcf9aa1a4fc
linda_forlizzi <la.for.lizzi@gmail.com>
2021-10-19 16:38:02 -0400

    render list and table

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container'
 // import SearchMeds from './components/SearchMeds';
-import UsersMedList from './components/UsersMedList';
+// import UsersMedList from './components/UsersMedList';
+import CreateMed from './components/CreateMed';
+import MedTable from './components/MedTable';
+import medsList from './data';
 
 const App = () => {
+
+  const [meds, setMeds] = useState(medsList);
+
+  const createMed = med => {
+      med._id = meds.length + 1;
+      setMeds([...meds, med]);
+  };
 
   // const [updating, setUpdating] = useState(false);
   // const initialMed = {_id: null, brandName: '', reaction: ''};
   // const [currentMed, setCurrentMed] = useState(initialMed);
 
   return (
-    <div className="usersMedList">
-      <UsersMedList />
-    </div>
+    <Container>
+      <div center className="container">
+        <CreateMed createMed={createMed} />
+        <h1>Medications with Known Possible Adverse Reactions</h1>
+        <h2>Dysphagia</h2>
+        {/* <UsersMedList /> */}
+        <MedTable meds={meds} />
+      </div>
+    </Container>
   );
 }
 
--- src/components/CreateMed.js
+++ src/components/CreateMed.js
@@ -1,34 +1,37 @@
-// this component adds a new medication when not found in SearchMeds and user clicks a button to create new med, returns it into a list--does it need to return into list?
+// this component adds a new medication and user clicks a button to create new med, returns it into a list BUT does it need to return into list????
 // the new med, once created by the user, needs to be added to the array and table
 import React, {useState} from 'react';
-import MedTable from './MedTable';
 import {ListGroup} from 'react-bootstrap';
+import MedTable from './MedTable';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+// import UsersMedList from './UsersMedList';
 
-// why aren't the props being picked up by MedTable?
 const CreateMed = (props) => {
-  const [meds, setMeds] = useState(medsList);
 
-  const createMed = med => {
-      med._id = meds.length + 1;
-      setMeds([...meds, med])
-    };
+  const createMed = {_id: null, genericName: '', brandName: ''};
+  const [med, setMed] = useState(createMed);
+
+  const handleChange = e => {
+    const {brandName, value} = e.target;
+    setMed({...med, [brandName]: value});
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (med.brandName && med.genericName && med.reaction) {
+      handleChange(e, props.NewMed(med));
+    }
+  }
 
   return (
-    <div className="viewMeds">
-      <MedTable meds={meds} />
-    </div>
-    
+    <ListGroup>
+      <ListGroup.Item onChange={handleChange}>{med.brandName}</ListGroup.Item>
+      <ListGroup.Item onChange={handleChange}>{med.genericName}</ListGroup.Item>
+      <ListGroup.Item onChange={handleChange}>{med.reaction}</ListGroup.Item>
+      <Button onClick={handleSubmit} variant="contained" sendIcon={<SendIcon />}>Add Medication</Button>
+    </ListGroup>
   )
 }
-  // return (
-  //   <ListGroup>
-  //     <ListGroup.Item onChange={handleChange}>{med.brandName}</ListGroup.Item>
-  //     <ListGroup.Item onChange={handleChange}>{med.genericName}</ListGroup.Item>
-  //     <ListGroup.Item onChange={handleChange}>{med.reaction}</ListGroup.Item>
-  //     <Button onClick={handleSubmit} variant="contained" sendIcon={<SendIcon />}>Add Medication</Button>
-  //   </ListGroup>
-  // )
 
 export default CreateMed;
