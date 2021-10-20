d492f9bf06a6de9f9bcc3c1895b3b41f8fb88c7b
linda_forlizzi <la.for.lizzi@gmail.com>
2021-10-20 19:49:20 -0400

    form state working

--- src/components/CreateMed.js
+++ src/components/CreateMed.js
@@ -1,50 +1,29 @@
 // this component adds a new medication and user clicks a button to create new med, returns it into a list BUT does it need to return into list????
 // the new med, once created by the user, needs to be added to the array and table
 import React, {useState} from 'react';
-// import {ListGroup} from 'react-bootstrap';
-// import MedTable from './MedTable';
 import Form from 'react-bootstrap/Form'
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
-// import UsersMedList from './UsersMedList';
 
 const CreateMed = (props) => {
 
   const createMed = {_id: null, brandName: "", genericName: "", reaction: ""};
-  const [createMedValue, setCreateMedValue] = useState(createMed);
-  // const [medValue, setMedValue] = useState(createMed);
-  // const [medValue, setMedValue] = useState("");
-
+  const [createMedValue, setCreateMedValue] = useState("");
+  
   const [brandNameValue, setBrandNameValue] = useState("");
   const [genericNameValue, setGenericNameValue] = useState("");
   const [reactionValue, setReactionValue] = useState("");
   
-  const handleMedChange = e => setCreateMedValue(e.target.value);
-  // const handleGenericNameChange = e => setGenericNameValue(e.target.value);
-  // const handleReactionChange = e => setReactionValue(e.target.value);
+  const handleBrandNameChange = e => setBrandNameValue(e.target.value);
+  const handleGenericNameChange = e => setGenericNameValue(e.target.value);
+  const handleReactionChange = e => setReactionValue(e.target.value);
     
-  const handleBrandNameSubmit = () => {
-    const brandNameData = {
-      name: brandNameValue
+  const handleMedSubmit = () => {
+    const createMedData = {
+      name: createMedValue
     }
-    setBrandNameValue("");
-    props.createBrandNameMed(brandNameData)
-  }
-
-  const handleGenericNameSubmit = () => {
-    const genericNameData = {
-      name: genericNameValue
-    }
-    setGenericNameValue("");
-    props.createGenericNameMed(genericNameData);
-  }
-
-  const handleReactionSubmit = () => {
-    const reactionData = {
-      name: reactionValue
-    }
-    setReactionValue("");
-    props.createReactionMed(reactionData);
+    setCreateMedValue("");
+    props.createMed(createMedData)
   }
 
   // TODO: cursor to next box on enter
@@ -54,20 +33,17 @@ const CreateMed = (props) => {
     <Form>
     <Form.Group controlId="medBrandName">
       <Form.Label value={brandNameValue}>Enter Brand Name:</Form.Label>
-      <Form.Control onChange={handleBrandNameSubmit} placeholder="Brand Name" />
+      <Form.Control onChange={handleBrandNameChange} placeholder="Brand Name" />
     </Form.Group>
     <Form.Group controlId="medGenericName">
       <Form.Label value={genericNameValue}>Enter Generic Name:</Form.Label>
-      <Form.Control onChange={handleGenericNameSubmit} placeholder="Generic Name" />
+      <Form.Control onChange={handleGenericNameChange} placeholder="Generic Name" />
     </Form.Group>
     <Form.Group controlId="medReaction">
       <Form.Label value={reactionValue}>Enter Reaction:</Form.Label>
-      <Form.Control onChange={handleReactionSubmit} placeholder="Severe Adverse Reaction" />
+      <Form.Control onChange={handleReactionChange} placeholder="Severe Adverse Reaction" />
     </Form.Group>
-      {/* <Form.Label onChange={handleChange}>{med.brandName}>Enter Brand Name</Form.Label>
-      <Form.Label onChange={handleChange}>{med.genericName}>Enter Generic Name</Form.Label>
-      <Form.Label onChange={handleChange}>{med.reaction}>Enter Reaction</Form.Label> */}
-    <Button onClick={handleMedChange} variant="contained" sendIcon={<SendIcon />}>Add Medication</Button>
+    <Button onClick={handleMedSubmit} variant="contained" sendIcon={<SendIcon />}>Add Medication</Button>
     </Form>
     </div>
   )
